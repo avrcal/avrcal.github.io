915b799c7ef7b3e07d3a718ccabe20085eb60165
avrcal <105925321+avrcal@users.noreply.github.com>
2026-03-02 15:35:17 +0300

    Delete src/app-directory directory

--- src/app-directory/src/index.ts
+++
@@ -1,6 +0,0 @@
-import AppDirectoryPage from "./components/pages/AppDirectoryPage";
-import patcher from "./stuff/patcher";
-
-export const onUnload = patcher();
-
-export const settings = AppDirectoryPage;
